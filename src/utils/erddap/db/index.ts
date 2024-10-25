b4dc394715f6a419f622d47d2d4a733dfc4b6b39
Anna Murphy <anna_murphy1@brown.edu>
2024-10-25 10:52:31 -0400

    remove erddap direct queries

--- src/utils/erddap/db/index.ts
+++
@@ -1,5 +0,0 @@
-export * as buoy from './buoy';
-export type { RiBuoyData, RiBuoyVariable } from './buoy';
-export * as db from './db';
-export * as fish from './fish';
-export type { Sample, Temperature, Species } from './fish';
