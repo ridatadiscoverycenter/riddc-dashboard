10c4d4c2db075b1b13743490ef9ba4650e709405
s-bessey <sam_bessey@brown.edu>
2025-01-09 13:33:25 -0500

    feat: add heatmap explainer text

--- src/app/datasets/massachusetts-buoys/page.tsx
+++ src/app/datasets/massachusetts-buoys/page.tsx
@@ -26,6 +26,36 @@ export default async function MassachusettsBuoyData({ searchParams }: PageProps)
       <Card className="bg-clear-900 col-span-2 items-center">
         <MaBuoySummary data={buoyData} />
       </Card>
+      <div className="col-span-3 flex flex-col justify-center">
+        <h2 className="font-header font-bold text-lg self-center">About this dataset</h2>
+        <p>
+          This dataset spans from 2017 to 2018 and was collected by the <LINKS.NBFSMN /> with{' '}
+          <LINKS.MassDEP /> as the lead agency. The heatmap above summarizes the number of
+          observations collected for each month for different variables. Use this heatmap to help
+          you decide what data you want to visualize or download. When you have an idea, go ahead
+          and select the buoys, variables and dates to explore. Or download the data in the most
+          appropriate format for your analyses! To begin, select a variable to see what data is
+          available.{' '}
+        </p>
+        <p className="self-left">
+          Note: The variables with &quot;Qualifier&quot; in the name provide annotations for the
+          corresponding variable without &quot;Qualifier&quot;. The qualifers are not plottable, but
+          can be downloaded for analysis offline.
+        </p>
+      </div>
     </div>
   );
 }
+
+const LINKS = {
+  NBFSMN: () => (
+    <ExternalLink href="https://dem.ri.gov/environmental-protection-bureau/water-resources/research-monitoring/narraganset-bay-assessment-fixed-site-monitoring">
+      Narragansett Bay Fixed Station Monitoring Network (NBFSMN)
+    </ExternalLink>
+  ),
+  MassDEP: () => (
+    <ExternalLink href="https://www.mass.gov/orgs/massachusetts-department-of-environmental-protections">
+      RIDEM-OWR
+    </ExternalLink>
+  ),
+};
