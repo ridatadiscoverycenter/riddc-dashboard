4f158640f80c895a5b3f4f3a0e96ef0a71ed17b1
s-bessey <sam_bessey@brown.edu>
2025-01-31 11:37:10 -0500

    fix: change comma separated list code

--- src/utils/fns/makeCommaSepList.ts
+++ src/utils/fns/makeCommaSepList.ts
@@ -2,9 +2,7 @@ export function makeCommaSepList(list: string[]) {
   if (list.length === 0) return '';
   if (list.length === 1) return list[0];
 
-  return list.reduce(
-    (prev, next, index) =>
-      `${prev}${index === 0 ? '' : `${index === list.length - 1 ? '' : ','} `}${index === list.length - 1 ? 'and ' : ''}${next}`,
-    ''
-  );
+  const listCopy = [...list];
+  const last = listCopy.pop();
+  return `${listCopy.join(', ')} and ${last}`;
 }
