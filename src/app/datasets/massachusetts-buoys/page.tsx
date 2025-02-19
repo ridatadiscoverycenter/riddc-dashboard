a2d0e32ebd6f36e71d0b6b00544a224db56d7852
Anna Murphy <anna_murphy1@brown.edu>
2025-02-19 14:38:52 -0500

    Reformat MA Buoys Page

--- src/app/datasets/massachusetts-buoys/page.tsx
+++ src/app/datasets/massachusetts-buoys/page.tsx
@@ -1,37 +1,92 @@
-import { Card, ExploreForm, ExternalLink, RiBuoyLocations, MaBuoySummary } from '@/components';
+import {
+  ExploreForm,
+  ExternalLink,
+  RiBuoyLocations,
+  MaBuoySummary,
+  BuoyPageSkeleton,
+  DataGraph,
+  GraphErrorPanel,
+  BuoyVariables,
+  DownloadBuoyData,
+} from '@/components';
 import { PageProps } from '@/types';
-import { fetchMaSummaryData, fetchMaBuoyCoordinates } from '@/utils/data/api/buoy';
-import { getParams } from '@/utils/fns/getParams';
-import { DataGraph } from './DataGraph';
+import { fetchWeatherData } from '@/utils/data';
+import { fetchMaSummaryData, fetchMaBuoyCoordinates, fetchMaBuoyData } from '@/utils/data/api/buoy';
+import { makeCommaSepList } from '@/utils/fns';
+import { ERROR_CODES, getParams } from '@/utils/fns/getParams';
 
 export default async function MassachusettsBuoyData({ searchParams }: PageProps) {
   const parsed = getParams(searchParams, 'ma');
   const buoyData = await fetchMaSummaryData();
   const buoyCoords = await fetchMaBuoyCoordinates();
 
-  return (
-    <div className="m-4 grid grid-cols-2 md:grid-cols-3 grid-flow-row gap-4">
-      <Card className="bg-clear-900 md:col-span-2 col-span-3 flex flex-col items-center justify-around gap-3">
-        <DataGraph params={parsed} buoys={buoyCoords} />
-      </Card>
-      <ExploreForm
-        buoys={buoyCoords}
-        location="ma"
-        dateBounds={{
-          startDate: new Date('2017-05-26'),
-          endDate: new Date('2018-1-09'),
-        }}
-        init={typeof parsed === 'string' ? undefined : parsed}
+  let graphBlock: React.ReactNode;
+  if (typeof parsed === 'string') {
+    graphBlock = (
+      <GraphErrorPanel
+        error={parsed === ERROR_CODES.NO_SEARCH_PARAMS ? undefined : parsed}
+        links={MA_BUOY_ERROR_LINKS}
       />
-      <div className="flex flex-col items-center justify-around col-span-1">
-        <h2 className="text-xl font-header font-bold">Where are these buoys?</h2>
-        <RiBuoyLocations locations={buoyCoords} />
-      </div>
-      <Card className="bg-clear-900 col-span-2 items-center">
-        <MaBuoySummary data={buoyData} />
-      </Card>
-      <div className="col-span-3 flex flex-col justify-center">
-        <h2 className="font-header font-bold text-lg self-center">About this dataset</h2>
+    );
+  } else {
+    const maBuoyData = await fetchMaBuoyData(parsed.buoys, parsed.vars, parsed.start, parsed.end);
+    const weatherData = await fetchWeatherData(parsed.start, parsed.end);
+    if (maBuoyData.length === 0) {
+      graphBlock = (
+        <GraphErrorPanel
+          error="No data is available given the selected parameters."
+          links={MA_BUOY_ERROR_LINKS}
+        />
+      );
+    } else {
+      graphBlock = (
+        <DataGraph
+          description={
+            <p className="text-black">
+              This plot compares {makeCommaSepList(parsed.vars)} between{' '}
+              {parsed.start.toLocaleDateString()} and {parsed.end.toLocaleDateString()} at{' '}
+              {makeCommaSepList(
+                parsed.buoys.map(
+                  (bid) => buoyCoords.find(({ buoyId }) => buoyId === bid)?.stationName || '???'
+                )
+              )}
+              . You can hover over the lines to see more specific data. The weather data below is
+              sourced from <ExternalLink href="https://www.rcc-acis.org/">NOAA</ExternalLink>.
+            </p>
+          }
+          weather={weatherData}
+          download={
+            <DownloadBuoyData
+              variables={parsed.vars}
+              region="ma"
+              buoys={parsed.buoys}
+              start={parsed.start}
+              end={parsed.end}
+            />
+          }
+        >
+          <BuoyVariables data={maBuoyData} height={200} />
+        </DataGraph>
+      );
+    }
+  }
+  return (
+    <BuoyPageSkeleton
+      graph={graphBlock}
+      form={
+        <ExploreForm
+          buoys={buoyCoords}
+          location="ma"
+          dateBounds={{
+            startDate: new Date('2017-05-26'),
+            endDate: new Date('2018-1-09'),
+          }}
+          init={typeof parsed === 'string' ? undefined : parsed}
+        />
+      }
+      map={<RiBuoyLocations locations={buoyCoords} />}
+      summary={<MaBuoySummary data={buoyData} />}
+      description={
         <p>
           This dataset spans from 2017 to 2018 and was collected by the <LINKS.NBFSMN /> with{' '}
           <LINKS.MassDEP /> as the lead agency. The heatmap above summarizes the number of
@@ -39,12 +94,23 @@ export default async function MassachusettsBuoyData({ searchParams }: PageProps)
           you decide what data you want to visualize or download. When you have an idea, go ahead
           and select the buoys, variables and dates to explore. Or download the data in the most
           appropriate format for your analyses! To begin, select a variable to see what data is
-          available.{' '}
+          available.
         </p>
-      </div>
-    </div>
+      }
+    />
   );
 }
+
+const MA_BUOY_ERROR_LINKS = [
+  {
+    href: '/datasets/massachusetts-buoys?buoys=bid101&vars=ChlorophyllSurface,ChlorophyllBottom&start=2017-06-01&end=2017-06-30',
+    description: 'Changes in Chlorophyll at Cole in June 2017',
+  },
+  {
+    href: '/datasets/massachusetts-buoys?buoys=bid102&vars=SalinityBottom,SalinitySurface&start=2018-05-01&end=2018-11-01',
+    description: 'Changes in Salinity at Taunton from May through October 2018',
+  },
+];
 
 const LINKS = {
   NBFSMN: () => (
