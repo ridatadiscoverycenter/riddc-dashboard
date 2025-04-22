2e8b9c2b8996879f1384d7365a95aa7a3b227ace
Anna Murphy <anna_murphy1@brown.edu>
2025-04-22 11:39:09 -0400

    domoic acid page blog post style

--- src/app/datasets/domoic-acid/page.tsx
+++ src/app/datasets/domoic-acid/page.tsx
@@ -1,13 +1,98 @@
-import { DomoicAcidMap } from '@/components';
+import { DomoicAcidMap, FullBleedColumn, Link } from '@/components';
 import { fetchDomoicAcidCoordinates, fetchDomoicAcidSample } from '@/utils/data/api/da';
 
 export default async function DomoicAcid() {
   const samples = await fetchDomoicAcidSample();
   const stations = await fetchDomoicAcidCoordinates();
+
   return (
-    <>
-      <h1>Domoic Acid</h1>
+    <FullBleedColumn className="my-2 gap-4">
+      <h1 className="text-xl">Domoic Acid Dataset</h1>
+      <p>
+        This map on domoic acid concentrations in Narragansett Bay (NB) was generated from a
+        research project carried out at the University of Rhode Island by Co- Principal
+        Investigators Drs. Matthew Bertin and Bethany Jenkins and their research teams. This work
+        was funded by Rhode Island Sea Grant and the Rhode Island Consortium for Coastal Ecology
+        Assessment, Innovation, and Modeling (RI C-AIM).
+      </p>
       <DomoicAcidMap samples={samples} stations={stations} />
-    </>
+      <h2 className="text-lg font-bold">Background</h2>
+      <p>
+        The project monitored species of the diatom{' '}
+        <span className="italic">Pseudo-nitzschia (P-n)</span> and their production of the
+        neurotoxin domoic acid at selected sites in NB, Rhode Island. While{' '}
+        <span className="italic">P-n</span> species have been present in NB for over 50 years, the
+        first Rhode Island shellfish harvest closure due to high levels of domoic acid in the
+        plankton was not until October 2016. This precautionary closure, another subsequent closure
+        in March 2017 due to domoic acid exceeding action limits in shellfish meat, and the
+        continued detection of low levels of domoic acid suggests that either{' '}
+        <span className="italic">P-n</span> species composition has changed to new toxic- producing
+        strains of <span className="italic">P-n</span> in NB, or NB environmental conditions have
+        shifted in favor of making resident <span className="italic">P-n</span> more toxic. The 2016
+        and 2017 closures sparked interest in identifying environmental drivers that lead to
+        increased domoic acid production as well as identifying <span className="italic">P-n</span>{' '}
+        species composition in NB.
+      </p>
+      <h2 className="text-lg font-bold">Methods</h2>
+      <p>
+        Over a two-year period from September 2017 &ndash; November 2019, our research group has
+        sampled NB weekly, collecting samples to measure various environmental factors such as
+        dissolved nutrients, chlorophyll a, temperature, and salinity from the surface seawater.
+        Additionally, we have monitored <span className="italic">P-n</span> species (through genetic
+        analysis of DNA sequences), and the cell-associated domoic acid from phytoplankton samples
+        using liquid chromatography-tandem mass spectrometry (LC- MS/MS). Our genetic methods can
+        distinguish between <span className="italic">P-n</span> species and our analytical methods
+        are very sensitive and can detect very low levels of domoic acid (ng domoic acid/L of
+        seawater).
+      </p>
+      <h2 className="text-lg font-bold">Results</h2>
+      <p>
+        We have found seasonal differences in <span className="italic">P-n</span> species in NB and
+        we have found a seasonal pattern in increased cell-associated domoic acid concentrations.
+        All toxin concentrations are at low levels, and no closures occurred during the sampling
+        period. Cell-associated domoic acid rises in the fall (September and October) and the late
+        spring/early summer (May and June), and highest concentrations were observed at the sites
+        closest to the entrance of NB. We hypothesize that these seasonally distinct{' '}
+        <span className="italic">P-n</span> communities are responsible for low-level domoic acid
+        production which likely contributed to the 2016 precautionary closure, while it appears a
+        species new to NB, Pseudo-nitzschia australis, likely contributed to the 2017 closure.{' '}
+        <span className="font-semibold">
+          This map shows how cell-associated domoic acid increases and decreases over the sampling
+          period. Sampling sites are shown with the concentration of domoic acid recorded on
+          specific dates.
+        </span>
+      </p>
+      <h2 className="text-lg font-bold">Impact</h2>
+      <p>
+        We hope this information will aid Rhode Island stakeholders by providing data following the
+        2016 and 2017 closures on when, where, and what concentration of low- level cell-associated
+        domoic acid is present. This is important as domoic acid can bioaccumulate in shellfish and
+        cause illness in humans when consumed. While cooking does not destroy the toxin, live
+        shellfish can naturally purge the toxin with time and become safe for consumption later.
+        These domoic acid measurements and <span className="italic">P-n</span> species composition
+        provide baseline data during a period of no closures for important context if additional
+        closures were to occur in the future.
+      </p>
+      <h2 className="text-lg font-bold">Acknowledgments</h2>
+      <p>
+        Thanks to Dr. Tatiana Rynearson, Jacob Strock, Jessica Carney, Nina Santos, Captain Stephen
+        Barber on the R/V Cap&apos;n Bert, Dr. Harold “Bud” Vincent II on the R/V Hope Hudner, and
+        Steve Granger on the R/V Zostera for assistance with sampling.
+      </p>
+      <p>
+        We thank graduate and undergraduate researchers and other research personnel Alexa Sterling,
+        Riley Kirk, Katherine Bell, Dr. Laura Holland, Marissa Caponi, Meagan King, Emily McDermith,
+        Erin Tully, Samantha Vaverka, Patrick Wilson, Stephanie Anderson, and Jorge Vazquez-Custodio
+        for sampling assistance.
+      </p>
+      <h2 className="text-lg font-bold">Learn More</h2>
+      <p>
+        The full dataset used to power this app is available{' '}
+        <Link href="https://pricaimcit.services.brown.edu/erddap/tabledap/da_4566_36f0_124a.html">
+          on ERDDAP
+        </Link>
+        . If using this data, please cite the ERDDAP dataset.
+      </p>
+    </FullBleedColumn>
   );
 }
--- src/components/visualizations/Maps/DomoicAcidMap.tsx
+++ src/components/visualizations/Maps/DomoicAcidMap.tsx
@@ -1,8 +1,10 @@
 'use client';
 import React from 'react';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import maplibregl from 'maplibre-gl';
+import { formatDate } from 'date-fns';
 
-import { Label } from '@/components';
+import buoymarker from '@/assets/buoy-marker.svg';
 import { DomoicAcidCoordinate, DomoicAcidSample } from '@/utils/data/api/da';
 import { useMap } from './useMap';
 
@@ -29,6 +31,10 @@ export function DomoicAcidMap({ samples, stations }: DomoicAcidMapProps) {
     () => Array.from(new Set(samples.map(({ normDA }) => normDA))),
     [samples]
   );
+  const rangeStations = React.useMemo(
+    () => Array.from(new Set(samples.map(({ stationName }) => stationName))),
+    [samples]
+  );
   const [selectedDateIndex, setSelectedDate] = React.useState(0);
   const [variable, setVariable] = React.useState<'pDA' | 'normDA'>('normDA');
   const selectedDate = React.useMemo(
@@ -36,9 +42,9 @@ export function DomoicAcidMap({ samples, stations }: DomoicAcidMapProps) {
     [sampleDates, selectedDateIndex]
   );
 
-  React.useEffect(() => {
-    if (loaded) {
-      const samplesAtDate = samples
+  const samplesAtDate = React.useMemo(
+    () =>
+      samples
         .filter(({ date }) => date.valueOf() === selectedDate.valueOf())
         .map((sample) => ({
           ...sample,
@@ -58,8 +64,30 @@ export function DomoicAcidMap({ samples, stations }: DomoicAcidMapProps) {
             normDA,
             pDA,
           },
-        }));
+        })),
+    [samples, selectedDate, stations]
+  );
 
+  const popup = React.useMemo(() => new maplibregl.Popup(), []);
+
+  React.useEffect(() => {
+    if (loaded) {
+      map.current.addSource('da-stations', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: stations.map((station) => ({
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [station.longitude, station.latitude],
+            },
+            properties: {
+              name: station.stationName,
+            },
+          })),
+        },
+      });
       map.current.addSource('da-samples', {
         type: 'geojson',
         data: {
@@ -67,6 +95,9 @@ export function DomoicAcidMap({ samples, stations }: DomoicAcidMapProps) {
           features: samplesAtDate,
         },
       });
+      const minVar = Math.min(...(variable === 'normDA' ? rangeNormDA : rangePDA));
+      const maxVar = Math.max(...(variable === 'normDA' ? rangeNormDA : rangePDA));
+      const midVar = (maxVar + minVar) / 2;
       map.current.addLayer({
         id: 'da-circles',
         type: 'circle',
@@ -76,50 +107,219 @@ export function DomoicAcidMap({ samples, stations }: DomoicAcidMapProps) {
             'interpolate',
             ['linear'],
             ['get', variable],
-            Math.min(...(variable === 'normDA' ? rangeNormDA : rangePDA)),
-            '#2ac780',
-            Math.max(...(variable === 'normDA' ? rangeNormDA : rangePDA)),
-            '#c70e36',
+            minVar,
+            '#00c951',
+            midVar,
+            '#fd9a00',
+            maxVar * 0.66,
+            '#c287bc',
           ],
           'circle-opacity': 0.75,
           'circle-radius': [
             'interpolate',
             ['linear'],
             ['get', variable],
-            Math.min(...(variable === 'normDA' ? rangeNormDA : rangePDA)),
-            20,
-            Math.max(...(variable === 'normDA' ? rangeNormDA : rangePDA)),
+            minVar,
+            10,
+            midVar,
+            25,
+            maxVar,
             40,
           ],
         },
       });
+
+      const buoyImg = new Image(45, 45);
+      buoyImg.src = buoymarker.src;
+      buoyImg.onload = () => {
+        map.current.addImage('buoy-marker', buoyImg);
+        map.current.addLayer({
+          id: 'da-buoys',
+          type: 'symbol',
+          source: 'da-stations',
+          layout: {
+            'icon-image': 'buoy-marker',
+            'icon-size': 0.5,
+          },
+        });
+        map.current.on('click', () => {
+          popup.remove();
+        });
+        map.current.on('click', 'da-buoys', (e: any) => {
+          const station = stations.find((station) => {
+            const { lng, lat } = e.lngLat;
+            const lngDiff = Math.abs(station.longitude - lng);
+            const latDiff = Math.abs(station.latitude - lat);
+            return lngDiff <= 0.005 && latDiff <= 0.005;
+          });
+          if (station) {
+            const sample = samplesAtDate.find(
+              (sample) => sample.properties.stationName === station.stationName
+            );
+            popup.setLngLat(e.lngLat);
+            popup.setHTML(
+              `<div style="display: flex; flex-flow: column; gap: 2px;"><h3 style="color: black; font-weight: bold">${station.stationName}</h3>${sample === undefined ? '' : `<p style="color: black">${Math.round(sample.properties.normDA * 1000) / 1000} ng of DA / L</p>`}</div>`
+            );
+            popup.addTo(map.current);
+          }
+        });
+      };
+
       return () => {
         map.current.removeLayer('da-circles');
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        map.current.removeLayer('da-buoys');
         map.current.removeSource('da-samples');
+        map.current.removeSource('da-stations');
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        map.current.removeImage('buoy-marker');
       };
     }
-  }, [map, loaded, samples, stations, selectedDate, rangeNormDA, rangePDA, variable]);
+  }, [
+    map,
+    loaded,
+    samples,
+    stations,
+    selectedDate,
+    rangeNormDA,
+    rangePDA,
+    variable,
+    popup,
+    samplesAtDate,
+  ]);
   return (
-    <div className="flex flex-col gap-2">
-      <div ref={containerRef} className="w-80 h-80 rounded-md" />
+    <>
+      <section className="full-bleed w-full min-h-[70vh] relative p-0 my-0">
+        <div ref={containerRef} className="absolute w-full h-full" />
+        {/* MD - Date Icon */}
+        <div className="md:hidden absolute top-[10%] left-8 bg-white/75 dark:bg-slate-800/75 flex text-lg font-bold p-4 rounded-md drop-shadow-md w-[225px]">
+          <h2>{formatDate(selectedDate, 'MM/dd/yyyy')}</h2>
+        </div>
+        {/* LG - Buoy List */}
+        <div className="hidden md:flex flex-col absolute top-[10%] left-8 h-[80%] bg-white/75 dark:bg-slate-800/75 p-4 rounded-md w-[225px] overflow-clip">
+          <h2 className="text-lg font-bold">{formatDate(selectedDate, 'MM/dd/yyyy')}</h2>
+          <ul className="flex flex-col gap-2">
+            {rangeStations.map((stationName) => {
+              const sample = samplesAtDate.find((s) => s.properties.stationName === stationName);
+              if (sample) {
+                return (
+                  <li key={stationName} className="flex flex-col">
+                    <h3>{stationName}</h3>
+                    <p className="text-sm text-slate-600 dark:text-slate-300">
+                      {Math.round(
+                        (variable === 'normDA' ? sample.properties.normDA : sample.properties.pDA) *
+                          1000
+                      ) / 1000}{' '}
+                      ng of DA / L of seawater
+                    </p>
+                    <div
+                      className={`rounded-sm not-sr-only h-3 ${getProgressBarClasses(
+                        variable === 'normDA' ? sample.properties.normDA : sample.properties.pDA,
+                        variable === 'normDA' ? rangeNormDA[0] : rangePDA[0],
+                        variable === 'normDA'
+                          ? rangeNormDA[rangeNormDA.length - 1]
+                          : rangePDA[rangePDA.length - 1]
+                      )} transition-all duration-200`}
+                    />
+                  </li>
+                );
+              }
+              return (
+                <li key={stationName} className="flex flex-col">
+                  <h3>{stationName}</h3>
+                  <p className="text-sm text-slate-600 dark:text-slate-300">No Data</p>
+                  <div
+                    className={`rounded-sm not-sr-only h-3 w-[1%] bg-red-400 transition-all duration-200`}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+      <div className="w-full">
+        <input
+          type="range"
+          className="w-full"
+          min={1}
+          max={sampleDates.length - 1}
+          step={1}
+          value={selectedDateIndex}
+          onChange={(evt) => setSelectedDate(evt.target.valueAsNumber)}
+          list="beep"
+        />
+        <datalist id="beep" className="flex flex-row w-full justify-between">
+          {[
+            1,
+            Math.ceil(sampleDates.length / 3),
+            Math.ceil((sampleDates.length * 2) / 3),
+            sampleDates.length,
+          ].map((index) => (
+            <option
+              key={index}
+              value={index - 1}
+              label={formatDate(sampleDates[index - 1], 'MM/dd/yyyy')}
+            >
+              {formatDate(sampleDates[index - 1], 'MM/dd/yyyy')}
+            </option>
+          ))}
+        </datalist>
+      </div>
+    </>
+  );
+}
+
+function getProgressBarClasses(value: number, min: number, max: number) {
+  const PROGRESS_BAR_WIDTHS = [
+    'w-[1%]',
+    'w-[10%]',
+    'w-[20%]',
+    'w-[30%]',
+    'w-[40%]',
+    'w-[50%]',
+    'w-[60%]',
+    'w-[70%]',
+    'w-[80%]',
+    'w-[90%]',
+    'w-[100%]',
+  ];
+  const PROGRESS_BAR_COLORS = [
+    'bg-green-500 dark:bg-green-300',
+    'bg-amber-500 dark:bg-amber-300',
+    'bg-red-500 dark:bg-red-400',
+  ]; //['bg-cyan-400', 'bg-amber-200', 'bg-green-300'];
+  const valuePercent = (value - min) / (max - min);
+  return `${PROGRESS_BAR_WIDTHS[Math.round(valuePercent * 10)]} ${PROGRESS_BAR_COLORS[Math.floor(valuePercent * 2.999)]}`;
+}
+
+type DateSelectorProps = {
+  value: number;
+  setValue: (newValue: number) => void;
+  max: number;
+  dateRange: { index: number; date: Date }[];
+  className: string;
+};
+
+function DateSelector({ value, setValue, max, dateRange }: DateSelectorProps) {
+  const id = React.useMemo(() => crypto.randomUUID(), []);
+  return (
+    <>
       <input
         type="range"
-        min={1}
-        max={sampleDates.length - 1}
-        step={1}
         className="w-full"
-        value={selectedDateIndex}
-        onChange={(evt) => setSelectedDate(evt.target.valueAsNumber)}
+        min={1}
+        max={max}
+        step={1}
+        value={value}
+        onChange={(evt) => setValue(evt.target.valueAsNumber)}
+        list={id}
       />
-      <Label label={variable}>
-        <input
-          type="checkbox"
-          checked={variable === 'normDA'}
-          onChange={() => setVariable((current) => (current === 'normDA' ? 'pDA' : 'normDA'))}
-        />
-      </Label>
-      <p>{selectedDate.toDateString()}</p>
-    </div>
+      <datalist id={id} className="flex flex-col justify-between items-center">
+        {dateRange.map(({ index, date }) => (
+          <option key={index} value={index} label={formatDate(date, 'MM/dd/yyyy')}>
+            {formatDate(date, 'MM/dd/yyyy')}
+          </option>
+        ))}
+      </datalist>
+    </>
   );
 }
