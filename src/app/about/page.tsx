db50a0ee967e3d629e810f3e0e8c580fa4b510ec
s-bessey <sam_bessey@brown.edu>
2025-05-06 13:12:10 -0400

    refactor: add paragraphs to about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,23 +9,30 @@ export default function About() {
         <h1 className="w-full text-center text-4xl font-header">
           About the Rhode Island Data Discovery Center
         </h1>
-        <p className="px-4 mx-8 text-xl">
-          The National Science Foundation in 2017 awarded the University of Rhode Island with a
-          grant to establish a statewide research consortium — the RI Consortium for Coastal Ecology
-          Assessment, Innovation, and Modeling (RI C-AIM) — to study the effects of climate
-          variability on coastal ecosystems. The RI Data Discovery Center is one of the efforts of
-          RI C-AIM consortium. The goal of the RI Data Discovery Center is to become the national
-          and international go-to-source for data on the Narragansett Bay ecosystem. For C-AIM
-          investigators pursuing the research goals of the Integrated Bay Observatory, Predicting
-          Ecosystem Response and Visualization & Imaging, RI Data Discovery Center will become the
-          site where they will store their data, share their data internally with other C-AIM
-          investigators and share their data externally with investigators around the world. In
-          addition to new data collected by C-AIM investigators, RI Data Discovery Center will also
-          collect and share historical data on the Narragansett Bay ecosystem. In addition to
-          sharing data with scientists, RIDDC will also become the go-to-source where decision
-          makers, land-use managers, relevant industries, citizen scientists and students can find
-          data on the Narragansett Bay ecosystem.
-        </p>
+        <div className="px-4 mx-6 text-xl">
+          <p className="my-4">
+            The National Science Foundation in 2017 awarded the University of Rhode Island with a
+            grant to establish a statewide research consortium — the RI Consortium for Coastal
+            Ecology Assessment, Innovation, and Modeling (RI C-AIM) — to study the effects of
+            climate variability on coastal ecosystems. The RI Data Discovery Center is one of the
+            efforts of RI C-AIM consortium.
+          </p>
+          <p className="my-4">
+            The goal of the RI Data Discovery Center is to become the national and international
+            go-to-source for data on the Narragansett Bay ecosystem. For C-AIM investigators
+            pursuing the research goals of the Integrated Bay Observatory, Predicting Ecosystem
+            Response and Visualization & Imaging, RI Data Discovery Center will become the site
+            where they will store their data, share their data internally with other C-AIM
+            investigators and share their data externally with investigators around the world.
+          </p>
+          <p className="my-4">
+            In addition to new data collected by C-AIM investigators, RI Data Discovery Center will
+            also collect and share historical data on the Narragansett Bay ecosystem. In addition to
+            sharing data with scientists, RIDDC will also become the go-to-source where decision
+            makers, land-use managers, relevant industries, citizen scientists and students can find
+            data on the Narragansett Bay ecosystem.
+          </p>
+        </div>
       </Card>
       <Card className="max-w-[1000px] bg-clear-800 dark:bg-clear-100 mb-8 py-6">
         <h2 className="w-full text-center text-4xl font-header">Credits</h2>
