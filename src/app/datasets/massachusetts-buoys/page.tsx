00910b34bfa98a7b0abf4b1c938420ccafcb477e
Anna Murphy <anna_murphy1@brown.edu>
2025-05-08 11:20:03 -0400

    Remove page wrapper

--- src/app/datasets/massachusetts-buoys/page.tsx
+++ src/app/datasets/massachusetts-buoys/page.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
-import { ExternalLink, DefaultBuoyPage } from '@/components';
+import {
+  BuoyPageSkeleton,
+  DefaultBuoyPage,
+  ExploreForm,
+  ExternalLink,
+  MaBuoySummary,
+  BuoyVariablesCard,
+  BuoyLocationsMap,
+} from '@/components';
+import { fetchWeatherData } from '@/utils/data';
+import {
+  fetchMaBuoyCoordinates,
+  fetchMaBuoyData,
+  fetchMaSummaryData,
+  MaBuoyViewerVariable,
+} from '@/utils/data/api/buoy';
+import {
+  ERROR_CODES,
+  extractParams,
+  fetchMulti,
+  makeCommaSepList,
+  parseParamBuoyIds,
+  parseParamBuoyVariablesMA,
+  parseParamDate,
+} from '@/utils/fns';
+
 import { PageProps } from '@/types';
-import { PageWrapper } from './PageWrapper';
 
 export default async function MassachusettsBuoyData({ searchParams }: PageProps) {
   return (
@@ -10,9 +34,80 @@ export default async function MassachusettsBuoyData({ searchParams }: PageProps)
       <PageWrapper
         params={searchParams}
         errorLinks={MA_BUOY_ERROR_LINKS}
-        description={DESCRIPTION}
       />
     </React.Suspense>
+  );
+}
+
+
+export async function PageWrapper({ params, errorLinks }: {
+  params: PageProps['searchParams'];
+  errorLinks: { href: string; description: string }[];
+}) {
+  const { buoyData, summaryData } = await fetchMulti({
+    buoyData: fetchMaBuoyCoordinates(),
+    summaryData: fetchMaSummaryData(),
+  });
+
+  const paramsOrError = extractParams(
+    {
+      buoys: parseParamBuoyIds(params ? params['buoys'] : undefined),
+      vars: parseParamBuoyVariablesMA(params ? params['vars'] : undefined),
+      start: parseParamDate(params ? params['start'] : undefined, 'start'),
+      end: parseParamDate(params ? params['end'] : undefined, 'end'),
+    },
+    [
+      ERROR_CODES.NO_BUOYS,
+      ERROR_CODES.NO_VARS,
+      ERROR_CODES.MISSING_START_DATE,
+      ERROR_CODES.MISSING_END_DATE,
+    ]
+  );
+
+  return (
+    <BuoyPageSkeleton
+      graph={
+        <BuoyVariablesCard
+          params={paramsOrError}
+          errorLinks={errorLinks}
+          buoyDataFetcher={(ids, vars, start, end) =>
+            fetchMaBuoyData(ids, vars as MaBuoyViewerVariable[], start, end)
+          }
+          region="ma"
+          weatherDataFetcher={fetchWeatherData}
+          description={
+            typeof paramsOrError === 'string' ? undefined : (
+              <>
+                This plot compares {makeCommaSepList(paramsOrError.vars)} between{' '}
+                {paramsOrError.start.toLocaleDateString()} and{' '}
+                {paramsOrError.end.toLocaleDateString()} at{' '}
+                {makeCommaSepList(
+                  paramsOrError.buoys.map(
+                    (bid) => buoyData.find(({ buoyId }) => buoyId === bid)?.stationName || '???'
+                  )
+                )}
+                . You can hover over the lines to see more specific data. The weather data below is
+                sourced from <ExternalLink href="https://www.rcc-acis.org/">NOAA</ExternalLink>.
+              </>
+            )
+          }
+        />
+      }
+      form={
+        <ExploreForm
+          buoys={buoyData}
+          location="ma"
+          dateBounds={{
+            startDate: new Date('2017-05-26'),
+            endDate: new Date('2018-1-09'),
+          }}
+          init={typeof paramsOrError === 'string' ? undefined : paramsOrError}
+        />
+      }
+      map={<BuoyLocationsMap locations={buoyData} />}
+      summary={<MaBuoySummary data={summaryData} />}
+      description={DESCRIPTION}
+    />
   );
 }
 
--- src/app/datasets/rhode-island-buoys/page.tsx
+++ src/app/datasets/rhode-island-buoys/page.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
-import { ExternalLink, DefaultBuoyPage } from '@/components';
+import {
+  BuoyPageSkeleton,
+  DefaultBuoyPage,
+  ExploreForm,
+  ExternalLink,
+  RiBuoySummary,
+  BuoyVariablesCard,
+  BuoyLocationsMap,
+} from '@/components';
 import { PageProps } from '@/types';
-import { PageWrapper } from './PageWrapper';
+import { fetchWeatherData } from '@/utils/data';
+import {
+  fetchRiBuoyCoordinates,
+  fetchRiBuoyData,
+  fetchRiSummaryData,
+  RiBuoyViewerVariable,
+} from '@/utils/data/api/buoy';
+import {
+  ERROR_CODES,
+  extractParams,
+  fetchMulti,
+  makeCommaSepList,
+  parseParamBuoyIds,
+  parseParamBuoyVariablesRI,
+  parseParamDate,
+} from '@/utils/fns';
 
 export default async function MassachusettsBuoyData({ searchParams }: PageProps) {
   return (
@@ -10,9 +33,79 @@ export default async function MassachusettsBuoyData({ searchParams }: PageProps)
       <PageWrapper
         params={searchParams}
         errorLinks={RI_BUOY_ERROR_LINKS}
-        description={DESCRIPTION}
       />
     </React.Suspense>
+  );
+}
+
+export async function PageWrapper({ params, errorLinks }: {
+  params: PageProps['searchParams'];
+  errorLinks: { href: string; description: string }[];
+}) {
+  const { buoyData, summaryData } = await fetchMulti({
+    buoyData: fetchRiBuoyCoordinates(),
+    summaryData: fetchRiSummaryData(),
+  });
+
+  const paramsOrError = extractParams(
+    {
+      buoys: parseParamBuoyIds(params ? params['buoys'] : undefined),
+      vars: parseParamBuoyVariablesRI(params ? params['vars'] : undefined),
+      start: parseParamDate(params ? params['start'] : undefined, 'start'),
+      end: parseParamDate(params ? params['end'] : undefined, 'end'),
+    },
+    [
+      ERROR_CODES.NO_BUOYS,
+      ERROR_CODES.NO_VARS,
+      ERROR_CODES.MISSING_START_DATE,
+      ERROR_CODES.MISSING_END_DATE,
+    ]
+  );
+
+  return (
+    <BuoyPageSkeleton
+      graph={
+        <BuoyVariablesCard
+          params={paramsOrError}
+          errorLinks={errorLinks}
+          buoyDataFetcher={(ids, vars, start, end) =>
+            fetchRiBuoyData(ids, vars as RiBuoyViewerVariable[], start, end)
+          }
+          region="ri"
+          weatherDataFetcher={fetchWeatherData}
+          description={
+            typeof paramsOrError === 'string' ? undefined : (
+              <>
+                This plot compares {makeCommaSepList(paramsOrError.vars)} between{' '}
+                {paramsOrError.start.toLocaleDateString()} and{' '}
+                {paramsOrError.end.toLocaleDateString()} at{' '}
+                {makeCommaSepList(
+                  paramsOrError.buoys.map(
+                    (bid) => buoyData.find(({ buoyId }) => buoyId === bid)?.stationName || '???'
+                  )
+                )}
+                . You can hover over the lines to see more specific data. The weather data below is
+                sourced from <ExternalLink href="https://www.rcc-acis.org/">NOAA</ExternalLink>.
+              </>
+            )
+          }
+        />
+      }
+      form={
+        <ExploreForm
+          buoys={buoyData}
+          location="ri"
+          dateBounds={{
+            startDate: new Date('2003-05-22'),
+            endDate: new Date('2019-12-31'),
+          }}
+          init={typeof paramsOrError === 'string' ? undefined : paramsOrError}
+        />
+      }
+      map={<BuoyLocationsMap locations={buoyData} />}
+      summary={<RiBuoySummary data={summaryData} />}
+      description={DESCRIPTION}
+    />
   );
 }
 
