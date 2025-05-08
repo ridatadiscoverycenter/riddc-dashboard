668977a7041fb67e77f12e3c8ffb44781f1d74ee
Anna Murphy <35873035+anna-murphy@users.noreply.github.com>
2025-05-08 12:10:01 -0400

    Stream Visualization Data To Client (#40)
    * scream MA buoy page data
    
    * fix params and refactor RI page
    
    * edits from meetings
    
    * helpers and typescript from review
    
    * Remove page wrapper
    
    * lint
    
    * remove unused import
    
    * add comments
    
    * lint

---
+++ src/components/visualizations/BuoyVariablesCard/BuoyVariablesCard.tsx
@@ -0,0 +1,74 @@
+'use server';
+
+import React from 'react';
+
+import { BuoyVariables, DataGraph, DownloadBuoyData, GraphErrorPanel } from '@/components';
+import type { WeatherData } from '@/utils/data';
+import type { MaBuoyData, MaBuoyViewerVariable, RiBuoyData } from '@/utils/data/api/buoy';
+import { ERROR_CODES } from '@/utils/fns';
+
+type BuoyVariablesProps = {
+  params: string | { buoys: string[]; vars: string[]; start: Date; end: Date };
+  region: 'ri' | 'ma';
+  errorLinks: { href: string; description: string }[];
+  buoyDataFetcher: (
+    buoys: string[],
+    variables: string[],
+    start: Date,
+    end: Date
+  ) => Promise<RiBuoyData[] | MaBuoyData[]>;
+  weatherDataFetcher: (start: Date, end: Date) => Promise<WeatherData[]>;
+  description: React.ReactNode;
+};
+
+export async function BuoyVariablesCard({
+  params,
+  region,
+  errorLinks,
+  buoyDataFetcher,
+  weatherDataFetcher,
+  description,
+}: BuoyVariablesProps) {
+  // If params parsed found no params or returned an error,
+  // display error in place of visualization.
+  if (typeof params === 'string') {
+    return (
+      <GraphErrorPanel
+        error={params === ERROR_CODES.NO_SEARCH_PARAMS ? undefined : params}
+        links={errorLinks}
+      />
+    );
+  }
+
+  const buoyData = await buoyDataFetcher(params.buoys, params.vars, params.start, params.end);
+
+  // If no data was found, display an error.
+  if (buoyData.length === 0) {
+    return (
+      <GraphErrorPanel
+        error="No data is available given the selected parameters."
+        links={errorLinks}
+      />
+    );
+  }
+
+  const weatherData = await weatherDataFetcher(params.start, params.end);
+
+  return (
+    <DataGraph
+      description={description}
+      weather={weatherData}
+      download={
+        <DownloadBuoyData
+          variables={params.vars as MaBuoyViewerVariable[]}
+          region={region}
+          buoys={params.buoys}
+          start={params.start}
+          end={params.end}
+        />
+      }
+    >
+      <BuoyVariables data={buoyData} height={200} />
+    </DataGraph>
+  );
+}
