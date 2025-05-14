3417ff7a6dce9bc729150bad0b9cbd0f81ad40c5
s-bessey <sam_bessey@brown.edu>
2025-05-14 15:03:39 -0400

    fix: do not export pagewrappers

--- src/app/datasets/massachusetts-buoys/page.tsx
+++ src/app/datasets/massachusetts-buoys/page.tsx
@@ -36,7 +36,7 @@ export default async function MassachusettsBuoyData({ searchParams }: PageProps)
   );
 }
 
-export async function PageWrapper({
+async function PageWrapper({
   params,
   errorLinks,
 }: {
