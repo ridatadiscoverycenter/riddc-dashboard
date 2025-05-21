364950bda1b1ceabcf52426231f1b82cde0823e8
Anna Murphy <anna_murphy1@brown.edu>
2025-05-21 11:28:03 -0400

    lint

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -27,14 +27,14 @@ export function Select({ label, options, forceLight = false, ...props }: SelectP
         options={formatted}
         unstyled
         classNames={{
-          control: ({ isFocused }) => `p-2 rounded-md shadow-sm bg-slate-100/80 text-black ${isFocused ? "border-teal-400" : ""} ${!forceLight ? "dark:bg-slate-800 dark:border-slate-600 dark:text-white" : "" }`,
-          placeholder: () => "text-slate-500 dark:text-slate-400",
+          control: ({ isFocused }) =>
+            `p-2 rounded-md shadow-sm bg-slate-100/80 text-black ${isFocused ? 'border-teal-400' : ''} ${!forceLight ? 'dark:bg-slate-800 dark:border-slate-600 dark:text-white' : ''}`,
+          placeholder: () => 'text-slate-500 dark:text-slate-400',
           menu: () => `mt-2 rounded-md p-2 bg-slate-100/90 dark:bg-slate-900/90`,
-          option: ({ isSelected }) => `m-1 ${isSelected ? "before:content-['✔_']" : ""}`,
-          multiValue: () => "m-1 px-2 gap-2 rounded-md border border-solid border-slate-500",
+          option: ({ isSelected }) => `m-1 ${isSelected ? "before:content-['✔_']" : ''}`,
+          multiValue: () => 'm-1 px-2 gap-2 rounded-md border border-solid border-slate-500',
         }}
       />
     </Label>
   );
 }
-    
