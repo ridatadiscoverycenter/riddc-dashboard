db57e55e4778121538d170841346a57606c0c761
Anna Murphy <anna_murphy1@brown.edu>
2025-05-22 16:05:59 -0400

    lint

--- src/utils/data/stream-gage/fetchStreamGageData.ts
+++ src/utils/data/stream-gage/fetchStreamGageData.ts
@@ -1,13 +1,12 @@
-"use server";
+'use server';
 
-import { StreamGageDataSchema } from "./types";
-
+import { StreamGageDataSchema } from './types';
 
 /**
  * Fetches stream gage data in Rhode Island for the given number of past days.
  * @param days The number of past days to fetch data for, including today.
  * @returns A list of gage heights for the specified time period.
- * 
+ *
  * Written by galenwinsor https://github.com/ridatadiscoverycenter/gage-viewer/blob/main/actions/getStreamGageData.ts
  */
 export async function fetchStreamGageData(days: number) {
@@ -19,10 +18,10 @@ export async function fetchStreamGageData(days: number) {
   const { error, data } = StreamGageDataSchema.safeParse(await response.json());
   if (error) {
     console.error(error);
-    throw new Error("An error occured parsing stream gage data!");
+    throw new Error('An error occured parsing stream gage data!');
   }
 
   return data.value.timeSeries.filter((timePoint) =>
-    timePoint.variable.variableName.includes("Gage height")
+    timePoint.variable.variableName.includes('Gage height')
   );
 }
