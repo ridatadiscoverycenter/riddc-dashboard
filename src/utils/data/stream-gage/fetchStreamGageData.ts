edc15ff4a93dbe6e8d128a7063fe621af3a0ccb9
Anna Murphy <anna_murphy1@brown.edu>
2025-05-22 16:51:28 -0400

    add page

--- src/utils/data/stream-gage/fetchStreamGageData.ts
+++ src/utils/data/stream-gage/fetchStreamGageData.ts
@@ -1,6 +1,36 @@
-'use server';
+import { z } from 'zod';
 
-import { StreamGageDataSchema } from './types';
+export const StreamGageDataSchema = z.object({
+  value: z.object({
+    timeSeries: z.array(
+      z.object({
+        sourceInfo: z.object({
+          siteName: z.string(),
+          geoLocation: z.object({
+            geogLocation: z.object({
+              latitude: z.number(),
+              longitude: z.number(),
+            }),
+          }),
+        }),
+        variable: z.object({
+          variableName: z.string(),
+        }),
+        values: z.array(
+          z.object({
+            value: z.array(
+              z.object({
+                value: z.string(),
+                dateTime: z.string(),
+              })
+            ),
+          })
+        ),
+      })
+    ),
+  }),
+});
+
 
 /**
  * Fetches stream gage data in Rhode Island for the given number of past days.
@@ -9,7 +39,7 @@ import { StreamGageDataSchema } from './types';
  *
  * Written by galenwinsor https://github.com/ridatadiscoverycenter/gage-viewer/blob/main/actions/getStreamGageData.ts
  */
-export async function fetchStreamGageData(days: number) {
+export async function fetchStreamGageData(days: number, variable: string) {
   // The instantaneous values service from USGS
   // Documentation: https://waterservices.usgs.gov/docs/instantaneous-values/instantaneous-values-details/
   const USGS_URL = `https://waterservices.usgs.gov/nwis/iv/?stateCd=ri&format=json&period=P${days}D`;
@@ -21,7 +51,29 @@ export async function fetchStreamGageData(days: number) {
     throw new Error('An error occured parsing stream gage data!');
   }
 
-  return data.value.timeSeries.filter((timePoint) =>
-    timePoint.variable.variableName.includes('Gage height')
-  );
+  return formatFetchedStreamGageData(data, variable);
 }
+
+function formatFetchedStreamGageData(
+  fetchedData: z.infer<typeof StreamGageDataSchema>,
+  variableName: string
+) {
+  const filteredData = fetchedData.value.timeSeries.filter((timePoint) =>
+    timePoint.variable.variableName.toLowerCase().includes(variableName.toLowerCase())
+  );
+
+  return filteredData.map((v) => {
+    const { siteName, geoLocation: { geogLocation: { longitude, latitude } } } = v.sourceInfo;
+    const { variableName } = v.variable;
+    const values = v.values.flat().map(({ value }) => value).flat().map(({ value, dateTime }) => ({ value: Number(value), dateTime: new Date(dateTime) }));
+    return {
+      siteName,
+      longitude,
+      latitude,
+      variableName,
+      values,
+    };
+  });
+}
+
+export type StreamGageData = ReturnType<typeof formatFetchedStreamGageData>;
