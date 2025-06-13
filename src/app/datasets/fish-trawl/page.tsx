4828be212db286249554ce592434e5eb327dd6cf
s-bessey <sam_bessey@brown.edu>
2025-06-13 11:00:51 -0400

    feat: grid for fish trawl

--- src/app/datasets/fish-trawl/page.tsx
+++ src/app/datasets/fish-trawl/page.tsx
@@ -1,3 +1,51 @@
-export default function FishTrawl() {
-  return <h1 className="w-full my-auto text-center text-2xl font-header">Coming Soon!</h1>;
+import { FishGrid } from '@/components/visualizations/Fish/FishGrid';
+import { fetchCoordinates, fetchSamples } from '@/utils/data/api/fish';
+import { fetchMulti } from '@/utils/fns';
+
+// fetch coordinates:
+// [
+//   {
+//     station_name: 'Whale Rock',
+//     longitude: -71.4208,
+//     latitude: 41.4395,
+//     stationName: 'Whale Rock'
+//   },
+//   {
+//     station_name: 'Fox Island',
+//     longitude: -71.4186,
+//     latitude: 41.5542,
+//     stationName: 'Fox Island'
+//   }
+// ]
+
+// Samples:
+// [
+//   {
+//     species: 'Alosa_spp',
+//     title: 'Alewife',
+//     station: 'Fox Island',
+//     year: 1959,
+//     abun: 0.141666667,
+//     animal: 'fish'
+//   },
+//   {
+//     species: 'Alosa_spp',
+//     title: 'Alewife',
+//     station: 'Fox Island',
+//     year: 1960,
+//     abun: 0.2625,
+//     animal: 'fish'
+//   },
+// ]
+
+export default async function FishTrawl() {
+  const { fishCoordinates, fishSamples } = await fetchMulti({
+    fishCoordinates: fetchCoordinates(),
+    fishSamples: fetchSamples(),
+  });
+  return (
+    <FishGrid
+      fishSamples={fishSamples.filter((sample) => sample.station === 'Whale Rock')}
+    ></FishGrid>
+  );
 }
--- src/utils/data/api/fish.ts
+++ src/utils/data/api/fish.ts
@@ -1,4 +1,4 @@
-import {
+import type {
   FetchedFishCoordinate,
   FetchedTemperature,
   FishCoordinate,
