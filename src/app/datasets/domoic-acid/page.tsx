30343172eea9f4dc0be5784d525261c9bfebab87
Anna Murphy <anna_murphy1@brown.edu>
2025-06-23 10:59:22 -0400

    change more h tags

--- src/app/datasets/domoic-acid/page.tsx
+++ src/app/datasets/domoic-acid/page.tsx
@@ -1,4 +1,4 @@
-import { DomoicAcidMap, FullBleedColumn, Link } from '@/components';
+import { DomoicAcidMap, FullBleedColumn, Header, Link } from '@/components';
 import { fetchDomoicAcidCoordinates, fetchDomoicAcidSample } from '@/utils/data/api/da';
 
 export default async function DomoicAcid() {
@@ -7,7 +7,9 @@ export default async function DomoicAcid() {
 
   return (
     <FullBleedColumn className="my-2 gap-4">
-      <h1 className="text-xl">Domoic Acid Dataset</h1>
+      <Header size="lg" variant="impact" tag="h1">
+        Domoic Acid Dataset
+      </Header>
       <p>
         This map on domoic acid concentrations in Narragansett Bay (NB) was generated from a
         research project carried out at the University of Rhode Island by Co- Principal
@@ -16,7 +18,7 @@ export default async function DomoicAcid() {
         Assessment, Innovation, and Modeling (RI C-AIM).
       </p>
       <DomoicAcidMap samples={samples} stations={stations} />
-      <h2 className="text-lg font-bold">Background</h2>
+      <Header size="md">Background</Header>
       <p>
         The project monitored species of the diatom{' '}
         <span className="italic">Pseudo-nitzschia (P-n)</span> and their production of the
@@ -33,7 +35,7 @@ export default async function DomoicAcid() {
         increased domoic acid production as well as identifying <span className="italic">P-n</span>{' '}
         species composition in NB.
       </p>
-      <h2 className="text-lg font-bold">Methods</h2>
+      <Header size="md">Methods</Header>
       <p>
         Over a two-year period from September 2017 &ndash; November 2019, our research group has
         sampled NB weekly, collecting samples to measure various environmental factors such as
@@ -45,7 +47,7 @@ export default async function DomoicAcid() {
         are very sensitive and can detect very low levels of domoic acid (ng domoic acid/L of
         seawater).
       </p>
-      <h2 className="text-lg font-bold">Results</h2>
+      <Header size="md">Results</Header>
       <p>
         We have found seasonal differences in <span className="italic">P-n</span> species in NB and
         we have found a seasonal pattern in increased cell-associated domoic acid concentrations.
@@ -62,7 +64,7 @@ export default async function DomoicAcid() {
           specific dates.
         </span>
       </p>
-      <h2 className="text-lg font-bold">Impact</h2>
+      <Header size="md">Impact</Header>
       <p>
         We hope this information will aid Rhode Island stakeholders by providing data following the
         2016 and 2017 closures on when, where, and what concentration of low- level cell-associated
@@ -73,7 +75,7 @@ export default async function DomoicAcid() {
         provide baseline data during a period of no closures for important context if additional
         closures were to occur in the future.
       </p>
-      <h2 className="text-lg font-bold">Acknowledgments</h2>
+      <Header size="md">Acknowledgments</Header>
       <p>
         Thanks to Dr. Tatiana Rynearson, Jacob Strock, Jessica Carney, Nina Santos, Captain Stephen
         Barber on the R/V Cap&apos;n Bert, Dr. Harold “Bud” Vincent II on the R/V Hope Hudner, and
@@ -85,7 +87,7 @@ export default async function DomoicAcid() {
         Erin Tully, Samantha Vaverka, Patrick Wilson, Stephanie Anderson, and Jorge Vazquez-Custodio
         for sampling assistance.
       </p>
-      <h2 className="text-lg font-bold">Learn More</h2>
+      <Header size="md">Learn More</Header>
       <p>
         The full dataset used to power this app is available{' '}
         <Link href="https://pricaimcit.services.brown.edu/erddap/tabledap/da_4566_36f0_124a.html">
