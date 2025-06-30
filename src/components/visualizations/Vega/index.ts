082ca547187a32d4e3a70ebb0e18fb5c424e1c62
Anna Murphy <anna_murphy1@brown.edu>
2025-06-30 12:25:32 -0400

    remove old weather history chart

--- src/components/visualizations/Vega/index.ts
+++ src/components/visualizations/Vega/index.ts
@@ -1,6 +1,5 @@
 export * from './RiBuoyMap';
 export * from './RiBuoySummary';
-export * from './WeatherHistory';
 export * from './BuoyVariables';
 export * from './MaBuoySummary';
 export * from './PlanktonSummary';
