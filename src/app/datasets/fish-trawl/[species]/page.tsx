f3dccc8f286983cbbdd69d810d03c247a378b531
s-bessey <sam_bessey@brown.edu>
2025-06-30 15:58:05 -0400

    refactor: add error links and remove unused code

--- src/app/datasets/fish-trawl/[species]/page.tsx
+++
@@ -1,7 +0,0 @@
-// import { fetchInfo } from '@/utils/data/api/fish';
-
-type InfoProps = { params: { species: string } };
-
-export default async function SpeciesInfo({ params }: InfoProps) {
-  return <p>Fish info for {params.species.replaceAll('%20', ' ')} coming soon!</p>;
-}
