f3dd00c750442a2f2474401b82dd4ef28b40f40f
s-bessey <sam_bessey@brown.edu>
2025-07-01 13:27:22 -0400

    refactor: make the fish trawl page smaller

--- src/app/datasets/fish-trawl/page.tsx
+++ src/app/datasets/fish-trawl/page.tsx
@@ -62,25 +62,23 @@ async function PageWrapper({
 
   return (
     <FullBleedColumn>
-      <div className="flex col-span-3 flex-col items-center justify-center">
-        <Header size="xl" variant="normal" className="mt-4">
-          Narragansett Bay Fish Trawl
-        </Header>
-        <p className="my-5">
-          The University of Rhode Island Graduate School of Oceanography Fish Trawl Survey is a
-          state funded survey of the bottom fish and invertebrate community in Narragansett Bay,
-          Rhode Island. The survey was initiated in 1959 by Charles J. Fish, founder and director of
-          the Narragansett Marine Laboratory, the precursor to the Graduate School of Oceanography.
-          The Fish Trawl Survey was developed to quantify the seasonal occurrences of migratory fish
-          populations, whereas scientists had previously relied on anecdotal information.
-        </p>
-        <p>
-          The heatmap below shows the abundance by year of the most commonly found fish in the
-          University of Rhode Island Graduate School of Oceanography Fish Trawl Survey. The icons
-          are sized and colored by abundance. To learn more about a species, select one from the{' '}
-          <Link href="#species-about">Explore Panel</Link>.
-        </p>
-      </div>
+      <Header size="xl" variant="normal" className="mt-4">
+        Narragansett Bay Fish Trawl
+      </Header>
+      <p className="my-5">
+        The University of Rhode Island Graduate School of Oceanography Fish Trawl Survey is a state
+        funded survey of the bottom fish and invertebrate community in Narragansett Bay, Rhode
+        Island. The survey was initiated in 1959 by Charles J. Fish, founder and director of the
+        Narragansett Marine Laboratory, the precursor to the Graduate School of Oceanography. The
+        Fish Trawl Survey was developed to quantify the seasonal occurrences of migratory fish
+        populations, whereas scientists had previously relied on anecdotal information.
+      </p>
+      <p>
+        The heatmap below shows the abundance by year of the most commonly found fish in the
+        University of Rhode Island Graduate School of Oceanography Fish Trawl Survey. The icons are
+        sized and colored by abundance. To learn more about a species, select one from the{' '}
+        <Link href="#species-about">Explore Panel</Link>.
+      </p>
       <div className="full-bleed ">
         <div className="grid grid-cols-2 md:grid-cols-3 grid-flow-row gap-4 m-4">
           <Card className="bg-white/90 md:col-span-2 col-span-3 row-span-2 flex flex-col items-center justify-around gap-3">
