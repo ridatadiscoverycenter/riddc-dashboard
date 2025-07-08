71ba639c4a5b98a1ecaceeba26810d90d09f119e
s-bessey <sam_bessey@brown.edu>
2025-07-08 11:04:37 -0400

    fix: close on select

--- src/components/ExploreForm/ExploreForm.tsx
+++ src/components/ExploreForm/ExploreForm.tsx
@@ -145,22 +145,24 @@ export function ExploreForm({
           .filter((variableOrUndefined) => variableOrUndefined !== undefined)}
       />
       <div className="w-full flex lg:flex-row flex-col gap-2 [&>label]:flex-1">
-        <Label label="Start">
+        <div>
+          <Label label="Start" />
           <CustomDatePicker
             selected={startDate}
             setDate={setStartDate}
             dateBounds={dateBounds}
             mode={mode}
           />
-        </Label>
-        <Label label="End">
+        </div>
+        <div>
+          <Label label="End" />
           <CustomDatePicker
             selected={endDate}
             setDate={setEndDate}
             dateBounds={dateBounds}
             mode={mode}
           />
-        </Label>
+        </div>
       </div>
       <Input
         type="submit"
