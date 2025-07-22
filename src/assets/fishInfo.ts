f882bd65c7cc1f7fc1c8c3e86607394f1d314248
s-bessey <sam_bessey@brown.edu>
2025-07-22 09:20:40 -0400

    make fishinfo into object

--- src/assets/fishInfo.ts
+++ src/assets/fishInfo.ts
@@ -1,156 +1,173 @@
-import { Info } from "@/types";
+import { Info } from '@/types';
 
 export const fishInfo: Record<string, Info> = {
-  "Alewife": {
-    "href": "http://www.fishbase.org/Summary/speciesSummary.php?ID=1583&AT=Alewife",
-    "name": "Alewife",
-    "sciName": "Alosa pseudoharengus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Alpse_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Clupeiformes (Herrings) > Clupeidae (Herrings, shads, sardines, menhadens) > Alosinae",
-    "IUCN": "Least Concern (LC)"
+  Alewife: {
+    href: 'http://www.fishbase.org/Summary/speciesSummary.php?ID=1583&AT=Alewife',
+    name: 'Alewife',
+    sciName: 'Alosa pseudoharengus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Alpse_u0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Clupeiformes (Herrings) > Clupeidae (Herrings, shads, sardines, menhadens) > Alosinae',
+    IUCN: 'Least Concern (LC)',
   },
-  "Atlantic Herring": {
-    "href": "http://www.fishbase.org/summary/speciesSummary.php?ID=24&AT=Atlantic+Herring",
-    "name": "Atlantic Herring",
-    "sciName": "Clupea harengus Linnaeus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Clhar_ub.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Clupeiformes (Herrings) > Clupeidae (Herrings, shads, sardines, menhadens) > Clupeinae",
-    "IUCN": "Least Concern (LC)"
+  'Atlantic Herring': {
+    href: 'http://www.fishbase.org/summary/speciesSummary.php?ID=24&AT=Atlantic+Herring',
+    name: 'Atlantic Herring',
+    sciName: 'Clupea harengus Linnaeus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Clhar_ub.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Clupeiformes (Herrings) > Clupeidae (Herrings, shads, sardines, menhadens) > Clupeinae',
+    IUCN: 'Least Concern (LC)',
   },
-  "Bluefish": {
-    "href": "http://www.fishbase.org/Summary/SpeciesSummary.php?ID=364&AT=Bluefish",
-    "name": "Bluefish",
-    "sciName": "Pomatomus saltatrix",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Posal_u9.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Scombriformes (Mackerels) > Pomatomidae (Bluefishes)",
+  Bluefish: {
+    href: 'http://www.fishbase.org/Summary/SpeciesSummary.php?ID=364&AT=Bluefish',
+    name: 'Bluefish',
+    sciName: 'Pomatomus saltatrix',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Posal_u9.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Scombriformes (Mackerels) > Pomatomidae (Bluefishes)',
 
-    "IUCN": "Vulnerable (VU)"
+    IUCN: 'Vulnerable (VU)',
   },
-  "Butterfish": {
-    "href": "http://www.fishbase.org/Summary/SpeciesSummary.php?ID=492&AT=butterfish",
-    "name": "Butterfish",
-    "sciName": "Peprilus triacanthus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Petri_u3.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Scombriformes (Mackerels) > Stromateidae (Butterfishes)",
-    "IUCN": "Not Evaluated"
+  Butterfish: {
+    href: 'http://www.fishbase.org/Summary/SpeciesSummary.php?ID=492&AT=butterfish',
+    name: 'Butterfish',
+    sciName: 'Peprilus triacanthus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Petri_u3.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Scombriformes (Mackerels) > Stromateidae (Butterfishes)',
+    IUCN: 'Not Evaluated',
   },
-  "Cunner": {
-    "href": "http://www.fishbase.org/Summary/speciesSummary.php?ID=3672&AT=Cunner",
-    "name": "Cunner",
-    "sciName": "Tautogolabrus adspersus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Taads_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Eupercaria/misc (Various families in series Eupercaria) > Labridae (Wrasses)",
-    "IUCN": "Least Concern (LC)"
+  Cunner: {
+    href: 'http://www.fishbase.org/Summary/speciesSummary.php?ID=3672&AT=Cunner',
+    name: 'Cunner',
+    sciName: 'Tautogolabrus adspersus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Taads_u0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Eupercaria/misc (Various families in series Eupercaria) > Labridae (Wrasses)',
+    IUCN: 'Least Concern (LC)',
   },
-  "Fourspot Flounder": {
-    "href": "http://www.fishbase.org/Summary/SpeciesSummary.php?ID=4229&AT=Fourspot+Flounder",
-    "name": "Fourspot Flounder",
-    "sciName": "Hippoglossina oblonga",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Paobl_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Pleuronectiformes (Flatfishes) > Paralichthyidae (Large-tooth flounders)",
-    "IUCN": "Least Concern (LC)"
+  'Fourspot Flounder': {
+    href: 'http://www.fishbase.org/Summary/SpeciesSummary.php?ID=4229&AT=Fourspot+Flounder',
+    name: 'Fourspot Flounder',
+    sciName: 'Hippoglossina oblonga',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Paobl_u0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Pleuronectiformes (Flatfishes) > Paralichthyidae (Large-tooth flounders)',
+    IUCN: 'Least Concern (LC)',
   },
-  "Horseshoe Crab": { "name": "Horseshoe Crab", "sciName": "Limulus polyphemus" },
-  "Lady Crab": { "name": "Lady Crab", "sciName": "Ovalipes ocellatus" },
-  "Little Skate": {
-    "href": "http://www.fishbase.org/summary/speciesSummary.php?ID=2557&AT=Little+Skate",
-    "name": "Little Skate",
-    "sciName": "Leucoraja erinacea",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Leeri_u0.jpg",
-    "Classification": "Elasmobranchii (sharks and rays) > Rajiformes (Skates and rays) > Rajidae (Skates)",
+  'Horseshoe Crab': { name: 'Horseshoe Crab', sciName: 'Limulus polyphemus' },
+  'Lady Crab': { name: 'Lady Crab', sciName: 'Ovalipes ocellatus' },
+  'Little Skate': {
+    href: 'http://www.fishbase.org/summary/speciesSummary.php?ID=2557&AT=Little+Skate',
+    name: 'Little Skate',
+    sciName: 'Leucoraja erinacea',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Leeri_u0.jpg',
+    Classification:
+      'Elasmobranchii (sharks and rays) > Rajiformes (Skates and rays) > Rajidae (Skates)',
 
-    "IUCN": "Near Threatened (NT)"
+    IUCN: 'Near Threatened (NT)',
   },
-  "Lobster": { "name": "Lobster", "sciName": "Homarus americanus" },
-  "Longhorn Sculpin": {
-    "href": "http://www.fishbase.org/summary/speciesSummary.php?ID=4120&AT=Longhorn+Sculpin",
-    "name": "Longhorn Sculpin",
-    "sciName": "Myoxocephalus octodecemspinosus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Myoct_u1.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Perciformes/Cottoidei (Sculpins) > Cottidae (Sculpins)",
+  Lobster: { name: 'Lobster', sciName: 'Homarus americanus' },
+  'Longhorn Sculpin': {
+    href: 'http://www.fishbase.org/summary/speciesSummary.php?ID=4120&AT=Longhorn+Sculpin',
+    name: 'Longhorn Sculpin',
+    sciName: 'Myoxocephalus octodecemspinosus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Myoct_u1.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Perciformes/Cottoidei (Sculpins) > Cottidae (Sculpins)',
 
-    "IUCN": "Not Evaluated"
+    IUCN: 'Not Evaluated',
   },
-  "Northern Searobin": {
-    "href": "http://www.fishbase.org/Summary/speciesSummary.php?ID=1243&AT=Northern+Searobin",
-    "name": "Northern Searobin",
-    "sciName": "Prionotus carolinus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Prcar_u3.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Perciformes/Scorpaenoidei (Scorpionfishes) > Triglidae (Searobins) > Prionotinae",
-    "IUCN": "Least Concern (LC)"
+  'Northern Searobin': {
+    href: 'http://www.fishbase.org/Summary/speciesSummary.php?ID=1243&AT=Northern+Searobin',
+    name: 'Northern Searobin',
+    sciName: 'Prionotus carolinus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Prcar_u3.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Perciformes/Scorpaenoidei (Scorpionfishes) > Triglidae (Searobins) > Prionotinae',
+    IUCN: 'Least Concern (LC)',
   },
-  "Red Hake": {
-    "href": "http://www.fishbase.org/summary/speciesSummary.php?ID=312&AT=Red+Hake",
-    "name": "Red Hake",
-    "sciName": "Urophycis chuss",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Urchu_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Gadiformes (Cods) > Phycidae (Phycid hakes)",
-    "IUCN": "Not Evaluated"
+  'Red Hake': {
+    href: 'http://www.fishbase.org/summary/speciesSummary.php?ID=312&AT=Red+Hake',
+    name: 'Red Hake',
+    sciName: 'Urophycis chuss',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Urchu_u0.jpg',
+    Classification: 'Actinopteri (ray-finned fishes) > Gadiformes (Cods) > Phycidae (Phycid hakes)',
+    IUCN: 'Not Evaluated',
   },
-  "Scup": {
-    "href": "http://www.fishbase.org/summary/speciesSummary.php?ID=452&AT=scup",
-    "name": "Scup",
-    "sciName": "Stenotomus chrysops",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Stchr_u1.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Eupercaria/misc (Various families in series Eupercaria) > Sparidae (Porgies)",
-    "IUCN": "Near Threatened (NT)"
+  Scup: {
+    href: 'http://www.fishbase.org/summary/speciesSummary.php?ID=452&AT=scup',
+    name: 'Scup',
+    sciName: 'Stenotomus chrysops',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Stchr_u1.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Eupercaria/misc (Various families in series Eupercaria) > Sparidae (Porgies)',
+    IUCN: 'Near Threatened (NT)',
   },
-  "Starfish": { "name": "Starfish", "sciName": "Asterias spp." },
-  "Silver Hake": {
-    "href": "http://www.fishbase.org/Summary/SpeciesSummary.php?ID=323&AT=Silver+Hake",
-    "name": "Silver Hake",
-    "sciName": "Merluccius bilinearis",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Mebil_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Gadiformes (Cods) > Merlucciidae (Merluccid hakes)",
-    "IUCN": "Near Threatened (NT)"
+  Starfish: { name: 'Starfish', sciName: 'Asterias spp.' },
+  'Silver Hake': {
+    href: 'http://www.fishbase.org/Summary/SpeciesSummary.php?ID=323&AT=Silver+Hake',
+    name: 'Silver Hake',
+    sciName: 'Merluccius bilinearis',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Mebil_u0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Gadiformes (Cods) > Merlucciidae (Merluccid hakes)',
+    IUCN: 'Near Threatened (NT)',
   },
-  "Spider Crabs": { "name": "Spider Crabs", "sciName": "Libinia emarginata" },
-  "Striped Searobin": {
-    "href": "http://www.fishbase.org/Summary/speciesSummary.php?ID=1244&AT=Striped+Searobin",
-    "name": "Striped Searobin",
-    "sciName": "Prionotus evolans",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Prevo_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Perciformes/Scorpaenoidei (Scorpionfishes) > Triglidae (Searobins) > Prionotinae",
-    "IUCN": "Least Concern (LC)"
+  'Spider Crabs': { name: 'Spider Crabs', sciName: 'Libinia emarginata' },
+  'Striped Searobin': {
+    href: 'http://www.fishbase.org/Summary/speciesSummary.php?ID=1244&AT=Striped+Searobin',
+    name: 'Striped Searobin',
+    sciName: 'Prionotus evolans',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Prevo_u0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Perciformes/Scorpaenoidei (Scorpionfishes) > Triglidae (Searobins) > Prionotinae',
+    IUCN: 'Least Concern (LC)',
   },
-  "Summer Flounder": {
-    "href": "http://www.fishbase.org/Summary/speciesSummary.php?ID=1338&AT=Summer+Flounder",
-    "name": "Summer Flounder",
-    "sciName": "Paralichthys dentatus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Paden_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Pleuronectiformes (Flatfishes) > Paralichthyidae (Large-tooth flounders)",
-    "IUCN": "Least Concern (LC)"
+  'Summer Flounder': {
+    href: 'http://www.fishbase.org/Summary/speciesSummary.php?ID=1338&AT=Summer+Flounder',
+    name: 'Summer Flounder',
+    sciName: 'Paralichthys dentatus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Paden_u0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Pleuronectiformes (Flatfishes) > Paralichthyidae (Large-tooth flounders)',
+    IUCN: 'Least Concern (LC)',
   },
-  "Tautog": {
-    "href": "http://www.fishbase.org/Summary/speciesSummary.php?ID=458&AT=Tautog",
-    "name": "Tautog",
-    "sciName": "Tautoga onitis",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Taoni_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Eupercaria/misc (Various families in series Eupercaria) > Labridae (Wrasses)",
-    "IUCN": "Vulnerable (VU)"
+  Tautog: {
+    href: 'http://www.fishbase.org/Summary/speciesSummary.php?ID=458&AT=Tautog',
+    name: 'Tautog',
+    sciName: 'Tautoga onitis',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Taoni_u0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Eupercaria/misc (Various families in series Eupercaria) > Labridae (Wrasses)',
+    IUCN: 'Vulnerable (VU)',
   },
-  "Weakfish": {
-    "href": "http://www.fishbase.org/Summary/SpeciesSummary.php?ID=406&AT=Weakfish",
-    "name": "Weakfish",
-    "sciName": "Cynoscion regalis",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Cyreg_u0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Eupercaria/misc (Various families in series Eupercaria) > Sciaenidae (Drums or croakers)",
-    "IUCN": "Endangered (EN)"
+  Weakfish: {
+    href: 'http://www.fishbase.org/Summary/SpeciesSummary.php?ID=406&AT=Weakfish',
+    name: 'Weakfish',
+    sciName: 'Cynoscion regalis',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Cyreg_u0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Eupercaria/misc (Various families in series Eupercaria) > Sciaenidae (Drums or croakers)',
+    IUCN: 'Endangered (EN)',
   },
-  "Windowpane Flounder": {
-    "href": "http://www.fishbase.org/Summary/speciesSummary.php?ID=530&AT=Windowpane+Flounder",
-    "name": "Windowpane Flounder",
-    "sciName": "Scophthalmus aquosus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Scaqu_l0.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Pleuronectiformes (Flatfishes) > Scophthalmidae (Turbots)",
-    "IUCN": "Not Evaluated"
+  'Windowpane Flounder': {
+    href: 'http://www.fishbase.org/Summary/speciesSummary.php?ID=530&AT=Windowpane+Flounder',
+    name: 'Windowpane Flounder',
+    sciName: 'Scophthalmus aquosus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Scaqu_l0.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Pleuronectiformes (Flatfishes) > Scophthalmidae (Turbots)',
+    IUCN: 'Not Evaluated',
   },
-  "Winter Flounder": {
-    "href": "http://www.fishbase.org/Summary/speciesSummary.php?ID=524&AT=winter+flounder",
-    "name": "Winter Flounder",
-    "sciName": "Pseudopleuronectes americanus",
-    "photoUrl": "https://www.fishbase.us/images/thumbnails/jpg/tn_Psame_u1.jpg",
-    "Classification": "Actinopteri (ray-finned fishes) > Pleuronectiformes (Flatfishes) > Pleuronectidae (Righteye flounders) > Pleuronectinae",
-    "IUCN": "Not Evaluated"
-  }
-}
+  'Winter Flounder': {
+    href: 'http://www.fishbase.org/Summary/speciesSummary.php?ID=524&AT=winter+flounder',
+    name: 'Winter Flounder',
+    sciName: 'Pseudopleuronectes americanus',
+    photoUrl: 'https://www.fishbase.us/images/thumbnails/jpg/tn_Psame_u1.jpg',
+    Classification:
+      'Actinopteri (ray-finned fishes) > Pleuronectiformes (Flatfishes) > Pleuronectidae (Righteye flounders) > Pleuronectinae',
+    IUCN: 'Not Evaluated',
+  },
+};
