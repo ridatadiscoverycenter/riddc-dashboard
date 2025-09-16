4fa22d4ba95eea950102f6cd52450d8cb2e572d6
s-bessey <sam_bessey@brown.edu>
2025-09-16 09:47:29 -0400

    refactor: use separate effect for api key fetching

--- src/components/visualizations/Maps/useMap.ts
+++ src/components/visualizations/Maps/useMap.ts
@@ -1,47 +1,53 @@
 import React from 'react';
 import maplibregl from 'maplibre-gl';
-import { fetchSecret } from '@/utils/fns/fetchSecret';
+import { fetchMapTilerSecret } from '@/utils/fns/fetchMapTilerSecret';
 
 export function useMap() {
   const containerRef = React.useRef<HTMLDivElement | null>(null);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const map = React.useRef<any>(null);
   const [loaded, setLoaded] = React.useState(false);
+  const [apiKey, setApiKey] = React.useState('');
 
   React.useEffect(() => {
-    async function createMap() {
-      // TO_REVIEW I think this is fetching the secret every time the map rerenders? Is that ok?
-      const API_KEY = await fetchSecret();
-      if (map.current) return;
-      if (!API_KEY) {
-        console.error('missing Maptiler API key');
-        return;
-      }
-      map.current = new maplibregl.Map({
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        container: containerRef.current as any,
-        style: `https://api.maptiler.com/maps/basic/style.json?key=${API_KEY}`,
-        // More things can be set here, but I'll ignore them for now.
-        // TODO: flexible bounds to work with fish trawl locations? Or just expand?
-        bounds: [
-          [-71.5, 41.92],
-          [-71.16, 41.32],
-        ],
-        center: [-71.4128, 41.584],
-        zoom: 8.5,
-        maxZoom: 11,
-        minZoom: 8,
-      });
-      function setLoadedOnMapLoad() {
-        setLoaded(true);
-      }
-      map.current.on('load', setLoadedOnMapLoad);
-      return () => {
-        map.current.off('load', setLoadedOnMapLoad);
-      };
+    async function getApiKey() {
+      const apiKey = await fetchMapTilerSecret();
+      setApiKey(apiKey);
     }
-    createMap();
-  }, [map, setLoaded]);
+    getApiKey();
+  }, []);
+
+  React.useEffect(() => {
+    const API_KEY = apiKey;
+    if (map.current) return;
+    if (API_KEY == '') return;
+    if (!API_KEY) {
+      console.error('missing Maptiler API key');
+      return;
+    }
+    map.current = new maplibregl.Map({
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      container: containerRef.current as any,
+      style: `https://api.maptiler.com/maps/basic/style.json?key=${API_KEY}`,
+      // More things can be set here, but I'll ignore them for now.
+      // TODO: flexible bounds to work with fish trawl locations? Or just expand?
+      bounds: [
+        [-71.5, 41.92],
+        [-71.16, 41.32],
+      ],
+      center: [-71.4128, 41.584],
+      zoom: 8.5,
+      maxZoom: 11,
+      minZoom: 8,
+    });
+    function setLoadedOnMapLoad() {
+      setLoaded(true);
+    }
+    map.current.on('load', setLoadedOnMapLoad);
+    return () => {
+      map.current.off('load', setLoadedOnMapLoad);
+    };
+  }, [map, setLoaded, apiKey]);
 
   return { containerRef, map, loaded };
 }
--- src/utils/fns/fetchMapTilerSecret.ts
+++ src/utils/fns/fetchMapTilerSecret.ts
@@ -4,7 +4,7 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const client = new SecretManagerServiceClient();
 
-export async function fetchSecret() {
+export async function fetchMapTilerSecret() {
   const [secret] = await client.accessSecretVersion({
     name: 'projects/766398966649/secrets/maptiler-key/versions/1',
   });
