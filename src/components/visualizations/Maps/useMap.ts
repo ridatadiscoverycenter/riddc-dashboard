2eaccd19b6e5603f100c77764ba922bfb0bb3e72
Anna Murphy <anna_murphy1@brown.edu>
2025-11-13 13:32:37 -0500

    update style link and remove api key

--- src/components/visualizations/Maps/useMap.ts
+++ src/components/visualizations/Maps/useMap.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import maplibregl, { type LngLatBoundsLike } from 'maplibre-gl';
-import { fetchMapTilerSecret } from '@/utils/fns/fetchMapTilerSecret';
+//import { fetchMapTilerSecret } from '@/utils/fns/fetchMapTilerSecret';
 
 const BOUNDS: LngLatBoundsLike = [
   [-71.5, 41.92],
@@ -12,24 +12,24 @@ export function useMap(bounds: LngLatBoundsLike = BOUNDS) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const map = React.useRef<any>(null);
   const [loaded, setLoaded] = React.useState(false);
-  const [apiKey, setApiKey] = React.useState(null);
+  //const [apiKey, setApiKey] = React.useState(null);
+
+  //React.useEffect(() => {
+  //  async function getApiKey() {
+  //    const apiKey = await fetchMapTilerSecret();
+  //    setApiKey(apiKey);
+  //  }
+  //  getApiKey();
+  //}, []);
 
   React.useEffect(() => {
-    async function getApiKey() {
-      const apiKey = await fetchMapTilerSecret();
-      setApiKey(apiKey);
-    }
-    getApiKey();
-  }, []);
-
-  React.useEffect(() => {
-    const API_KEY = apiKey;
+    //const API_KEY = apiKey;
     if (map.current) return;
-    if (!API_KEY) return;
+    //if (!API_KEY) return;
     map.current = new maplibregl.Map({
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       container: containerRef.current as any,
-      style: `https://api.maptiler.com/maps/basic/style.json?key=${API_KEY}`,
+      style: "https://tiles.openfreemap.org/styles/liberty", //`https://api.maptiler.com/maps/basic/style.json?key=${API_KEY}`,
       // More things can be set here, but I'll ignore them for now.
       // TODO: flexible bounds to work with fish trawl locations? Or just expand?
       bounds: bounds,
@@ -45,7 +45,7 @@ export function useMap(bounds: LngLatBoundsLike = BOUNDS) {
     return () => {
       map.current.off('load', setLoadedOnMapLoad);
     };
-  }, [map, apiKey, setLoaded, bounds]);
+  }, [map, setLoaded, bounds]);
 
   return { containerRef, map, loaded };
 }
