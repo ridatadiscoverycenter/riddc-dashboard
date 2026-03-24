977ff68335907435a3d1764e4ceaf70c2f0f1f28
Anna Murphy <anna_murphy1@brown.edu>
2026-03-24 17:07:19 -0400

    Add dataset exploration params

--- src/app/datasets/osom/explorer/page.tsx
+++ src/app/datasets/osom/explorer/page.tsx
@@ -6,7 +6,7 @@ export default async function OsomExplorer() {
       <Header size="lg" variant="impact" tag="h1">
         OSOM Explorer
       </Header>
-      <OsomExporerMap rasterUrl="https://qa-tile-server.riddc.brown.edu/services/ocean_his_4384_temp@1/tiles/{z}/{x}/{y}.png" />
+      <OsomExporerMap variable={'salt'} rasterIndex={0} />
     </FullBleedColumn>
   );
 }
