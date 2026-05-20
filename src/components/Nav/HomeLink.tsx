80b62bce1b4151207b306dcdb0cd1ae49741d37b
tdivoll <tiothy_divoll@brown.edu>
2026-05-20 11:03:43 -0400

    add external link to uri rinest page

--- src/components/Nav/HomeLink.tsx
+++ src/components/Nav/HomeLink.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { usePathname } from 'next/navigation';
-import { Link, RiNestLogo } from '@/components';
+import { Link, ExternalLink, RiNestLogo } from '@/components';
 
 type HomeLinkProps = {
   className?: string;
@@ -25,7 +25,9 @@ export function HomeLink({ className = '' }: HomeLinkProps) {
 function LinkContents() {
   return (
     <h1 className="flex flex-row items-center gap-4 text-2xl">
-      <RiNestLogo size={4.5} variant="white" />
+      <ExternalLink href="https://web.uri.edu/rinsfepscor/rinest/">
+        <RiNestLogo size={4.5} variant="white" />
+      </ExternalLink>
       <span className="sm:flex hidden whitespace-nowrap">Rhode Island Data Discovery Center</span>
       <span className="sm:hidden">RIDDC</span>
     </h1>
