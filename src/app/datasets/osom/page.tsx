87c7e32e5e1cc163a71124d0f585d3a3ed1a7607
tdivoll <tiothy_divoll@brown.edu>
2026-07-28 08:47:17 -0400

    fix: add margin so second paragragh is obvious

--- src/app/datasets/osom/page.tsx
+++ src/app/datasets/osom/page.tsx
@@ -150,10 +150,10 @@ const DESCRIPTION = (
       available by the year at 1.5 hour increments. As the model covers the entire Narragansett Bay,
       data is always available at all buoy locations.
     </p>
-    <p>
+    <p style={{ marginTop: '1rem' }}>
       Looking for the source data? You can download the{' '}
       <ExternalLink href="https://app.globus.org/file-manager?origin_id=fa54d7a3-ea6e-4c80-9486-8d3c2c99d838&origin_path=%2F">
-        ROMS forcing files 
+        ROMS forcing files
       </ExternalLink>{' '}
       used to build the model or the{' '}
       <ExternalLink href="https://app.globus.org/file-manager?origin_id=6579142b-9d21-4ab1-b541-e0c471e1e098&origin_path=%2F">
